6769f631d03f1ecf4522ae902679cdaf3a5a3d57
Garrett Breach <breachg1992@gmail.com>
2020-10-30 15:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor((Math.random() *100) +1);
+ var itemObject = {};
+ itemObject[item] = itemPrice
+ cart.push(itemObject);
+ return(`${item} has been added to your cart.`)
+ return cart;
 }
 
 function viewCart() {
